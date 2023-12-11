81a42f78c03fef94a777ef2769cfaac5f96afb17
Seong-Jun1525 <sjsj123455@naver.com>
2023-12-11 11:49:35 +0900

    프로젝트 생성및 전체구조, layout&component 폴더 구조 생성

---
+++ README.md
@@ -0,0 +1,16 @@
+# react-note-app
+## 필요패키지
+```
+dayjs
+html-react-parser
+@reduxjs/toolkit
+react-quill
+uuid
+react-toastify
+styled-components
+react-icons
+react-router-dom
+react-redux
+
+--save-dev @types/styled-components @types/uuid
+```
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+export {default as AllNotes} from "./AllNotes/AllNotes"
+export {default as ArchiveNotes} from "./ArchiveNotes/ArchiveNotes"
+export {default as TrashNotes} from "./TrashNotes/TrashNotes"
+export {default as TagNotes} from "./TagNotes/TagNotes"
+export {default as ErrorPage} from "./ErrorPage/ErrorPage"
+
+// 이렇게 하면 아래처럼 import할 수 있다
+// import {
+//     AllNotes,
+//     ArchiveNotes,
+//     TrashNotes,
+//     TagNotes,
+//     ErrorPage
+// } from "./pages"
